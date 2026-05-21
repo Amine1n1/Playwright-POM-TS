3707cc99ad854810319a0b979b76e272669231c1
Amine Gaddah <aminegaddah@192.168.1.11>
2026-05-21 13:11:06 +0100

    final update

--- tests/login.fail.spec.ts
+++ tests/login.fail.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '../fixtures/pom.fixture';
 
-test('Deliberate failure: wrong flash message', async ({ pm }) => {
+test('fail login', async ({ pm }) => {
   await pm.loginPage.openLoginPage();
   await pm.loginPage.userLogin('fusername', 'fpass');
-  await pm.securePage.assertSuccess();
+  await pm.loginPage.assertFailedUsername();
 });
